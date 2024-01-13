65d026060b8601975c4eebb743390f2b2d18ce17
-akku- <151517253+akku1139@users.noreply.github.com>
2024-01-13 18:51:04 +0900

    8

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,20 @@ export interface Env {
 }
 
 function v1(req: Request): Promise<Response> {
-  req.url = decodeURIComponent(new URL(req.url).searchParams.get("url"));
+  let req2 = req
+  req2.url = decodeURIComponent(new URL(req.url).searchParams.get("url"));
   console.log(req);
   return fetch(req);
 }
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-    try{
     const url:URL = new URL(request.url);
     switch (url.pathname) {
       case "/v1":
         return v1(req);
       default:
         break;
-    }
-    } catch(e){
-      console.log(e)
     }
 	}
 };
